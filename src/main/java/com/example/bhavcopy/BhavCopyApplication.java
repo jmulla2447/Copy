2dda8333bdb9f50de3a36d10abccde6e5a3832c7
javedmulla <javed.java18@gmail.com>
2024-10-16 21:22:21 +0200

    base commit

---
+++ src/main/java/com/example/bhavcopy/BhavCopyApplication.java
@@ -0,0 +1,13 @@
+package com.example.bhavcopy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BhavCopyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BhavCopyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bhavcopy/BhavCopyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bhavcopy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BhavCopyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
