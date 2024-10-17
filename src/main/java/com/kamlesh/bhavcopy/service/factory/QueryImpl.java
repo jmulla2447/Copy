6175cfe858e2be54f6b9f66660979ebd8dc8f03a
javedmulla <javed.java18@gmail.com>
2024-10-17 21:32:29 +0200

    5 Refactor code and QueryImpl use Enum

--- src/main/java/com/kamlesh/bhavcopy/service/factory/QueryImpl.java
+++ src/main/java/com/kamlesh/bhavcopy/service/factory/QueryImpl.java
@@ -1,9 +1,6 @@
 package com.kamlesh.bhavcopy.service.factory;
 
-import com.kamlesh.bhavcopy.service.strategy.CountQueryStrategy;
-import com.kamlesh.bhavcopy.service.strategy.GainQueryStrategy;
 import com.kamlesh.bhavcopy.service.strategy.QueryStrategy;
-import com.kamlesh.bhavcopy.service.strategy.SymbolQueryStrategy;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,16 +8,7 @@ public class QueryImpl implements QueryFactory {
 
     @Override
     public QueryStrategy getStrategy(String queryType) {
-        switch (queryType.toUpperCase()) {
-            case "SYMBOL":
-                return new SymbolQueryStrategy();
-            case "COUNT":
-                return new CountQueryStrategy();
-            case "GAIN":
-                return new GainQueryStrategy();
-            // Add other strategies here as needed
-            default:
-                throw new IllegalArgumentException("Unknown query type: " + queryType);
-        }
+        QueryType type = QueryType.fromString(queryType);
+        return type.getStrategy();
     }
 }
---
+++ src/main/java/com/kamlesh/bhavcopy/service/factory/QueryType.java
@@ -0,0 +1,30 @@
+package com.kamlesh.bhavcopy.service.factory;
+
+import com.kamlesh.bhavcopy.service.strategy.CountQueryStrategy;
+import com.kamlesh.bhavcopy.service.strategy.GainQueryStrategy;
+import com.kamlesh.bhavcopy.service.strategy.QueryStrategy;
+import com.kamlesh.bhavcopy.service.strategy.SymbolQueryStrategy;
+
+public enum QueryType {
+    SYMBOL(new SymbolQueryStrategy()),
+    COUNT(new CountQueryStrategy()),
+    GAIN(new GainQueryStrategy());
+
+    private final QueryStrategy strategy;
+
+    QueryType(QueryStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public QueryStrategy getStrategy() {
+        return strategy;
+    }
+
+    public static QueryType fromString(String queryType) {
+        try {
+            return QueryType.valueOf(queryType.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("Unknown query type: " + queryType);
+        }
+    }
+}
